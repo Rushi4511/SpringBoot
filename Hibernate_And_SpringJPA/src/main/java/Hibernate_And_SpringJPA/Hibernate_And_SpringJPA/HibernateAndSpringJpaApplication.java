868897a1fb17e5f0e4bdd81d63fadf6a1a2120db
Rushi4511 <rushikarle45@gmail.com>
2024-12-04 19:57:59 +0530

    Hibernate and Spring JPA 3.1

---
+++ Hibernate_And_SpringJPA/src/main/java/Hibernate_And_SpringJPA/Hibernate_And_SpringJPA/HibernateAndSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package Hibernate_And_SpringJPA.Hibernate_And_SpringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAndSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAndSpringJpaApplication.class, args);
+	}
+
+}
---
+++ Hibernate_And_SpringJPA/src/test/java/Hibernate_And_SpringJPA/Hibernate_And_SpringJPA/HibernateAndSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package Hibernate_And_SpringJPA.Hibernate_And_SpringJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateAndSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
