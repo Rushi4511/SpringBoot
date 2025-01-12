f1e2b4479ec96974402f3113398d923e412eccbe
Rushi4511 <rushikarle45@gmail.com>
2025-01-12 23:43:27 +0530

    Spring Testing :Understanding JUnit and AssertJ : Week 7.1 & 7.2 & 7.3

--- SpringTesting/src/test/java/com/SpringTesting/SpringTestingApplicationTests.java
+++ SpringTesting/src/test/java/com/SpringTesting/SpringTestingApplicationTests.java
@@ -1,13 +1,81 @@
 package com.SpringTesting;
 
-import org.junit.jupiter.api.Test;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringTestingApplicationTests {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+@Slf4j
+class TestingAppApplicationTests {
+
+	@BeforeEach
+	void setUp() {
+		log.info("Starting the method, setting up config");
+	}
+
+	@AfterEach
+	void tearDown() {
+		log.info("Tearing down the method");
+	}
+
+	@BeforeAll
+	static void setUpOnce() {
+		log.info("Setup Once...");
+	}
+
+	@AfterAll
+	static void tearDownOnce() {
+		log.info("Tearing down all...");
+	}
 
 	@Test
-	void contextLoads() {
+//	@Disabled
+	void testNumberOne() {
+		int a = 5;
+		int b = 3;
+
+		int result = addTwoNumbers(a, b);
+
+//		Assertions.assertEquals(8, result);
+//		Assertions.
+
+//		assertThat(result)
+//				.isEqualTo(7)
+//				.isCloseTo(9, Offset.offset(1));
+
+//		assertThat("Apple")
+//				.isEqualTo("Apple")
+//				.startsWith("App")
+//				.endsWith("l")
+//				.hasSize(3);
+
+	}
+
+	@Test
+//	@DisplayName("displayTestNameTwo")
+	void testDivideTwoNumbers_whenDenominatorIsZero_ThenArithmeticException() {
+
+		int a = 5;
+		int b = 0;
+
+		assertThatThrownBy(() -> divideTwoNumbers(a, b))
+				.isInstanceOf(ArithmeticException.class)
+				.hasMessage("Tried to divide by zero");
+
+	}
+
+	int addTwoNumbers(int a, int b) {
+		return a+b;
+	}
+
+	double divideTwoNumbers(int a, int b) {
+		try {
+			return a/b;
+		} catch (ArithmeticException e) {
+			log.error("Arithmentic excepiton occured: "+e.getLocalizedMessage());
+			throw new ArithmeticException("Tried to divide by zero");
+		}
 	}
 
 }
