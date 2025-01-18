57fdf3a1966eb63d450f42b3ed6d641df2cba107
Rushi4511 <rushikarle45@gmail.com>
2025-01-18 23:51:39 +0530

    Spring Testing: Testing JaCoCO :Week 7.7

--- SpringTesting/src/test/java/com/SpringTesting/TestContainerConfiguration.java
+++ SpringTesting/src/test/java/com/SpringTesting/TestContainerConfiguration.java
@@ -10,7 +10,7 @@ import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestContainerConfiguration {
-
+    // To Work This Container Docker Should be On
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgreSQLContainer(){
